90a257bf2ebec0e7d6bd991e4f8b10e98e0588f7
Celina <celinazhou27@gmail.com>
2021-08-30 12:47:02 -0400

    Adding blood calculator code

---
+++ blood_calculator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 12:45:49 2021
+
+@author: celin
+"""
+def interface():
+	print("Blood Calculator")
+	print("Make a choice")
+	print("9 - Quit")
+	choice = input("Make a choice: ")
+	print(choice)
+	return choice
+
+interface()

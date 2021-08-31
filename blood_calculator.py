f16b7fefb67d45e38ef59e84b2cfbf3b83106efa
Celina <celinazhou27@gmail.com>
2021-08-31 11:56:40 -0400

    Adding LDL analysis to blood_calculator to determine if the inputted LDL value is normal, borderline high, high, or very high

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,12 +12,15 @@ def interface():
         print("Make a Choice")
         print("1 - HDL Analysis")
         print("9 - Quit")
+        print("3 - LDL Analysis")
         choice = int(input("Make a Choice: "))
         print(type(choice))
         if choice == 9:
             keep_running = False
         elif choice == 1:
             HDL_Driver()
+        elif choice == 3:
+            LDL_Driver()
     print(choice)
     return choice
     
@@ -40,6 +43,29 @@ def hdl_analysis(HDL_value):
 
 def hdl_output(HDL_value, HDL_answer):
     print("The HDL value of {} is considered {}".format(HDL_value, HDL_answer))
+
+def LDL_Driver():
+    LDL_value = ldl_input()
+    LDL_character = ldl_analysis(LDL_value)
+    ldl_output(LDL_value, LDL_character)
+    
+def ldl_input():
+    ldl_value = int(input(("Enter LDL Value: ")))
+    return ldl_value
+
+def ldl_analysis(LDL_value):
+    if LDL_value >= 190:
+        return "Very high"
+    elif 160 <= LDL_value <= 189:
+        return "High"
+    elif 130 <= LDL_value <= 159:
+        return "Borderline High"
+    else:
+        return "Normal"
+
+def ldl_output(LDL_value, LDL_answer):
+    print("The LDL value of {} is considered {}".format(LDL_value, LDL_answer))
+
 
 
 

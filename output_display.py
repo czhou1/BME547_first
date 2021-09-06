21625e4c4a3a92ff428189d0dc0a59c734d3f0d2
Celina <celinazhou27@gmail.com>
2021-09-06 12:06:20 -0400

    Add output_display file with readability code from class schedule

---
+++ output_display.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 12:04:06 2021
+
+@author: celin
+"""
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))

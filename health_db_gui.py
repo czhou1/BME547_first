8cc72de3b72b685e8619715e79e6d4e56f4f0e8b
Celina <celinazhou27@gmail.com>
2021-11-07 22:53:12 -0500

    modifications to files from 11/3

--- health_db_gui.py
+++ health_db_gui.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
+from health_db_client import add_patient_to_server
 
-def create_output(name, patient_id, blood_letter, rh_factor):
+
+def create_output(name, p_id, blood_letter, rh_factor, center):
     out_string = 'Patient name: {}\n'.format(name)
     out_string += 'Blood Type: {}{}\n'.format(blood_letter, rh_factor)
-    return out_string
+    out_string += 'Donation Center: {}\n'.format(center)
+    answer = add_patient_to_server(name, p_id,
+                          "{}{}".format(blood_letter, rh_factor))
+    return out_string, answer
 
 
 def design_window():
@@ -13,15 +18,17 @@ def design_window():
     def ok_button_cmd():
         # Get needed data from GUI
         name = name_data.get()
-        patient_id = id_data.get()
+        p_id = id_data.get()
         blood_letter = blood_letter_data.get()
         rh_factor = rh_data.get()
+        center = donation_center_data.get()
         
         # Call external function to do the work that can be tested
-        answer = create_output(name, patient_id, blood_letter, rh_factor)
+        out_string, answer = create_output(name, p_id, blood_letter, rh_factor, center)
         
         # Update GUI
-        print(answer)
+        print(out_string)
+        output_string.configure(text=answer)
     
     def cancel_cmd():
         root.destroy()
@@ -30,19 +37,19 @@ def design_window():
     root.title("Health Database GUI")
 
     top_label = ttk.Label(root, text='Blood Donor Database')
-    top_label.grid(column=0, row=0, columnspan=2)
+    top_label.grid(column=0, row=0, columnspan=2, sticky='w')
 
     ttk.Label(root, text='Name').grid(column=0, row=1, sticky='e')
 
     name_data = tk.StringVar()
-    name_entry_box = ttk.Entry(root, width=50, textvariable=name_data)
-    name_entry_box.grid(column=1, row=1)
+    name_entry_box = ttk.Entry(root, width=40, textvariable=name_data)
+    name_entry_box.grid(column=1, row=1, sticky='w', columnspan=2)
 
     ttk.Label(root, text='ID').grid(column=0, row=2)
 
     id_data = tk.StringVar()
     id_entry_box = ttk.Entry(root, width=10, textvariable=id_data)
-    id_entry_box.grid(column=1, row=2)
+    id_entry_box.grid(column=1, row=2, sticky='w')
 
     blood_letter_data = tk.StringVar()
     ttk.Radiobutton(root, text='A', variable=blood_letter_data,
@@ -55,10 +62,22 @@ def design_window():
                     value='O').grid(column=0, row=6, sticky=tk.W)
 
     rh_data = tk.StringVar()
+    rh_data.set('-')
     rh_checkbox = ttk.Checkbutton(root, text='RH Positive',
                                   variable=rh_data, onvalue='+',
                                   offvalue='-')
     rh_checkbox.grid(column=1, row=4)
+    
+    ttk.Label(root, text='Nearest Donation Center').grid(column=3, row=0)
+
+    donation_center_data = tk.StringVar()
+    combo_box = ttk.Combobox(root, textvariable=donation_center_data)
+    combo_box['values'] = ('Durham', 'Raleigh', 'Cary', 'Apex')
+    combo_box.state(['readonly'])
+    combo_box.grid(column=3, row=1)
+    
+    output_string = ttk.Label(root)
+    output_string.grid(column=0, row=10)
 
     ok_button = ttk.Button(root, text='Ok', command=ok_button_cmd)
     ok_button.grid(column=1, row=6)
@@ -68,6 +87,7 @@ def design_window():
 
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     design_window()
